92211fe9ee77afb5bd3135201b884f1a933ee656
jackson218 <wbjackson218@gmail.com>
2018-12-03 10:37:40 -0700

    added comment after URI

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_heroku import Heroku
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = ""
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://gitkmvhthnlnhg:71d2b868373e45d75d00ab9e73ae17476bac22df8ca85b919016e3967426c519@ec2-184-72-239-186.compute-1.amazonaws.com:5432/dc7vpvjksm002t
+"
+# Not secure, I know. Just practicing for project
 heroku = Heroku(app)
 db = SQLAlchemy(app)
 
