4e7f2bf215cfe4f77842826355eb791d5bcd2b5a
jackson218 <wbjackson218@gmail.com>
2018-12-03 09:52:39 -0700

    pre heroku push

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_heroku import Heroku
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = ""
+heroku = Heroku(app)
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return "<h1>Welcome</h1>"
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
